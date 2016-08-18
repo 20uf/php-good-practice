dd7ba2fbb0839e55a50d116e6829985e10bd7c61
Michael COULLERET <michael@coulleret.pro>
2016-08-18 13:50:02 +0200

    Update FacebookClientTest.php

--- tests/guzzle/FacebookClientTest.php
+++ tests/guzzle/FacebookClientTest.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * This file is part of the Social Live project.
+ * This file is part of the php good practice démo.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
